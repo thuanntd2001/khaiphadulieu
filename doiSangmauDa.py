e22531ae286d3d4ab39979fdb2f14b7679032208
thuanntd2001 <thiensudanduong12@gmail.com>
2023-04-21 05:46:18 +0700

    sua data

--- doiSangmauDa.py
+++ doiSangmauDa.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import random
-df = pd.read_csv('dataTrain.csv')
+df = pd.read_csv('dataTuLam.csv')
 
 
 #1:Phi,2:Uc,3:Au,4:A
 lstxoa=[]
 
-for i in range(754):
+for i in range(381):
 
     if df.iloc[i, 3] <10:
-        df.iloc[i, 3] = "Trắng sáng"
+        df.iloc[i, 3] = "Trắng Sáng"
     elif df.iloc[i, 3] <15:
-        df.iloc[i, 3] = "Trắng"
+        df.iloc[i, 3] = "Trắng Vàng"
     elif df.iloc[i, 3] <27:
-        df.iloc[i, 3] = "Nâu Vàng"
+        df.iloc[i, 3] = "Nâu Vừa"
     else:
         df.iloc[i, 3] = "Nâu Sậm"
 
